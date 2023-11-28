c98903c260b2c90263cce69c87f407e79ada45b9
Arsyaq Fery <renggajulianto@gmail.com>
2023-11-28 14:24:01 +0700

    first commit

---
+++ Config/DatabaseConfig.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Config;
+
+class DatabaseConfig
+{
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database_name = "webmodul5";
+    public $port = 3306;
+
+
+}
